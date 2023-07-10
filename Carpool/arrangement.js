7c416ef5aba1389c4d53f9804aaf7fa672ef885e
Nolle <nolle.everaert@student.kuleuven.be>
2023-07-10 17:58:38 +0200

    Minimize amount of drivers

--- Carpool/arrangement.js
+++ Carpool/arrangement.js
@@ -22,19 +22,25 @@ export function arrangeSeats(people) {
   const max = cars.length;
   for (let i = 0; i < passengers.length; i++) {
     let count = 0;
-    while (cars[carIndex][1] === 0) {
-      if (count > max - 1) {
-        return "Niet genoeg beschikbare plaatsen";
-      }
+    try {
+      while (cars[carIndex][1] === 0) {
+        if (count > max - 1) {
+          return "Niet genoeg beschikbare plaatsen";
+        }
 
-      carIndex = (carIndex + 1) % cars.length;
-      count++;
+        carIndex++;
+        count++;
+      }
+    } catch (e) {
+      return "Niet genoeg beschikbare plaatsen";
     }
     cars[carIndex][1]--;
     cars[carIndex].push(passengers[i][0]);
-    carIndex = (carIndex + 1) % cars.length;
   }
 
+  cars = optimizeDrivers(cars);
+
+  // Remove the number of seats from each car
   for (let j = 0; j < cars.length; j++) {
     const backupCar = [];
     for (let i = 0; i < cars[j].length; i++) {
@@ -49,7 +55,39 @@ export function arrangeSeats(people) {
   for (let i = 0; i < cars.length; i++) {
     const driver = cars[i][0];
     const passengers = cars[i]; // Include the driver
-    result += `${driver}'s auto: ${passengers.join(", ")}` + "\n";
+    result += `${driver}'s auto: ${passengers.join(", ")}` + "\n" + "\n";
   }
   return result;
+}
+
+function optimizeDrivers(drivers) {
+  let seatsNeeded = 0;
+  for (let i = 0; i < drivers.length; i++) {
+    seatsNeeded += drivers[i].length - 1;
+  }
+
+  // We know how many seats we need, now we need to minimize the number of drivers
+  let count = 0;
+  const actualDrivers = [];
+  while (count < seatsNeeded) {
+    const currentCar = drivers.shift();
+    count += currentCar.length - 1 + currentCar[1];
+    actualDrivers.push(currentCar);
+  }
+
+  // Assign passengers to cars
+  let carIndex = 0;
+  const max = cars.length;
+  for (let i = 0; i < drivers.length; i++) {
+    while (actualDrivers[carIndex][1] === 0) {
+      carIndex++;
+    }
+    actualDrivers[carIndex][1]--;
+    actualDrivers[carIndex].push(drivers[i][0]);
+    for (let i = 2; i < drivers[i].length; i++) {
+      actualDrivers[carIndex].push(drivers[i][i]);
+    }
+  }
+
+  return actualDrivers;
 }
